6099b13a2777002a786c229a6ee5283635c636a0
Swarangi Sonawane <swarangisonawane1@gmail.com>
2026-09-20 00:11:54 +0530

    Add Register Page

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from routes.home import home
 from routes.login import login
+from routes.register import register
 app=Flask(__name__)
 app.register_blueprint(home)
 app.register_blueprint(login)
+app.register_blueprint(register)
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ routes/register.py
@@ -0,0 +1,5 @@
+from flask import Blueprint,render_template
+register=Blueprint("register",__name__)
+@register.route("/register")
+def register_page():
+    return render_template("register.html")
